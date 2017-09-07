86692dfd14f43c9251f0fee7df7b85ce51157fa1
Ben McElroy <ben.mcelroy.uk@gmail.com>
2017-09-07 15:35:33 +0100

    Formalised day 5

---
+++ 2015/Day5/solution.ps1
@@ -0,0 +1,18 @@
+﻿$inputs = get-content -Path "D:\git\AdventOfCode\2015\Day5\input.txt"
+$rule1 = "[aeiou].*[aeiou].*[aeiou]"
+$rule2 = "([a-z])\1"
+$rule3 = "ab|cd|pq|xy" 
+$rule4 = "(..).*\1"
+$rule5 = "(.).\1"
+function Get-Part1 ($inputs)
+{
+    $Count = ($inputs | ? {$_ -notmatch $rule3 -and $_ -match $rule1 -and $_ -match $rule2} | Measure).Count
+    return $Count
+}
+function Get-Part2 ($inputs)
+{
+    $Count = ($inputs | ? {$_ -match $rule4 -and $_ -match $rule5} | Measure).Count
+    return $count
+}
+Get-Part1 -inputs $inputs
+Get-Part2 -inputs $inputs
