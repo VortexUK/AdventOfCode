7033b7d546342a0e8242a7bc6f4f295f4e969f84
Ben McElroy <ben.mcelroy.uk@gmail.com>
2017-09-07 15:30:21 +0100

    formalised day 4

---
+++ 2015/Day4/solution.ps1
@@ -0,0 +1,37 @@
+﻿$inputs = get-content -Path "D:\git\AdventOfCode\2015\Day4\input.txt"
+$md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+$utf8 = new-object -TypeName System.Text.UTF8Encoding
+function Get-Part1 ($inputs)
+{
+    $hashnotfound = $true
+    $count = 0
+    while ($hashnotfound)
+    {
+        $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes("$inputs$count")))
+        if ($hash -match "^00-00-0")
+        {
+            $hashnotfound = $false
+            return $count
+        }
+        $count++
+    }
+}
+function Get-Part2 ($inputs)
+{
+    $hashnotfound = $true
+    $count = 0
+    while ($hashnotfound)
+    {
+        $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes("$inputs$count")))
+        if ($hash -match "^00-00-00")
+        {
+            $hashnotfound = $false
+            return $count
+        }
+        $count++
+    }
+}
+Get-Part1 -inputs $inputs
+Get-Part2 -inputs $inputs
+
+
