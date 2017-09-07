ed24b6dbd4996e7a3027424410ea0cd964689ff2
Ben McElroy <ben.mcelroy.uk@gmail.com>
2017-09-07 19:55:29 +0100

    Formalised Day 8

---
+++ 2015/Day8/solution.ps1
@@ -0,0 +1,28 @@
+$inputs = get-content -Path "D:\git\AdventOfCode\2015\Day8\input.txt"
+$escapes = @('\\\\(?!".)','\\"(?=.)','\\(x[a-z0-9]{2})')
+
+function Get-Part1 ($inputs)
+{
+    $actualstrings = @()
+    foreach ($string in $inputs)
+    {
+        $actualstrings += ((($string -replace $escapes[0],'a') -replace $escapes[1],'b') -replace $escapes[2],'c') -replace '"'
+    }
+    $stringsize = (($actualstrings -join '') -split '' | measure).Count
+    $inputsize = (($inputs -join '') -split '' | measure).Count
+    return ($inputsize - $stringsize)
+}
+function Get-Part2 ($inputs)
+{
+    $extendedstrings = @()
+    foreach ($string in $inputs)
+    {
+        
+        $extendedstrings += "`"$(($string -replace "\\","\\")-replace '"','\"')`""
+    }
+    $extendedsize = (($extendedstrings -join '') -split '' | measure).Count
+    $inputsize = (($inputs -join '') -split '' | measure).Count
+    return ($extendedsize - $inputsize)
+}
+Get-Part1 -inputs $inputs
+Get-Part2 -inputs $inputs
