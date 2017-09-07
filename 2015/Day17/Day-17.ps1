3ae22a0013cf67060b08de0c92e4a68056237fcc
Ben McElroy <ben.mcelroy.uk@gmail.com>
2017-09-07 14:52:32 +0100

    Adding everything i have currently

---
+++ 2015/Day17/Day-17.ps1
@@ -0,0 +1,57 @@
+﻿$inp = gc "C:\Users\bmcel\OneDrive\Documents\PowerShellChallenge\Day-17_input.txt"
+$count = 1
+$goodcombos = @()
+function Test-Value ($start, $inputs)
+{
+    $count = 0
+    $index = 0
+    foreach ($value in $inputs)
+    {
+        $testvalue = $start + $Value
+        if ($testvalue -eq 150)
+        {
+            $combo = (((compare-object $inp $inputs).InputObject) + $value | sort ) -join ','
+            if ($script:goodcombos -notcontains $combo)
+            {
+                $count++
+                $script:goodcombos += $combo
+                Write-Host "Found one!"
+                
+            }
+            return $count
+        }
+        if ($testvalue -gt 150)
+        {
+            return $count
+        }
+        $inputlength = ($inputs | Measure-Object).Count 
+        if ($inputlength -gt 1)
+        {
+            $newinputs = New-Object System.Collections.ArrayList($null)
+            for ($i=0; $i -lt $inputlength; $i++)
+            {
+                if($i -ne $index)
+                {
+                    $null = $newinputs.add($inputs[$i])
+                }
+            }
+            $count += (Test-Value -start $testvalue -inputs $newinputs)
+        }
+        $index++
+    }
+    return $count
+}
+
+
+
+$currentcount = ($goodcombos | ? {$_ -notmatch "40|18"} | measure).Count
+$currentcount +=  ($goodcombos | ? {$_ -match "18.*18" -and $_ -notmatch "40"} | measure).Count # only both 18s
+$currentcount +=  ($goodcombos | ? {$_ -match "18" -and $_ -notmatch "18.*18" -and $_ -notmatch "40"} | measure).Count * 2# 1 18 no 40s
+$currentcount +=  ($goodcombos | ? {$_ -match "18" -and $_ -notmatch "18.*18" -and $_ -match "40"} | measure).Count * 4# 1 18 1 40
+$currentcount +=  ($goodcombos | ? {$_ -match "18.*18" -and $_ -match "40"} | measure).Count *2 # 2 18 1 40
+$currentcount +=  ($goodcombos | ? {$_ -notmatch "18" -and $_ -match "40"} | measure).Count *2 # no 18 1 40
+
+
+
+$currentcount +=  ($goodcombos | ? {$_ -match "40.*40" -and $_ -notmatch "18"} | measure).Count # only both 40s
+$currentcount +=  ($goodcombos | ? {$_ -match "40.*40" -and $_ -match "18(?!.*18)"} | measure).Count # #40s + 1 18
---
+++ 2015/Day2/Day-2.ps1
@@ -0,0 +1,28 @@
+﻿$presents = get-content D:\presents.txt
+
+$total = 0
+$totalribbon = 0
+
+foreach ($present in $presents)
+{
+    [int[]]$presentsplit = $present -split 'x'
+    $volume = $presentsplit[0] * $presentsplit[1] * $presentsplit[2]
+    $side1 = $presentsplit[0] * $presentsplit[1]
+    $side2 = $presentsplit[0] * $presentsplit[2]
+    $side3 = $presentsplit[1] * $presentsplit[2]
+    $smallestside = $side1
+    $smallestperimeter = ($presentsplit[0] * 2) + ($presentsplit[1] * 2)
+    if ($smallestside -gt $side2)
+    {
+        $smallestside = $side2
+        $smallestperimeter = ($presentsplit[0] * 2) + ($presentsplit[2] * 2)
+    }
+    if ($smallestside -gt $side3)
+    {
+        $smallestside = $side3
+        $smallestperimeter = ($presentsplit[1] * 2) + ($presentsplit[2] * 2)
+    }
+    $total += ($side1 *2) + ($side2 *2) + ($side3 *2) +$smallestside
+    $totalribbon += ($smallestperimeter + $volume)
+    
+}
