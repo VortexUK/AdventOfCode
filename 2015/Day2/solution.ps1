56cff26e35bea00990ba47fc6b43aa1d03be5eb2
Ben McElroy <ben.mcelroy.uk@gmail.com>
2017-09-07 15:12:16 +0100

    Formalised Day 2

---
+++ 2015/Day2/solution.ps1
@@ -0,0 +1,29 @@
+﻿$inputs = get-content -Path "D:\git\AdventOfCode\2015\Day2\input.txt"
+function Get-Part1 ($inputs)
+{
+    $total = 0
+    foreach ($present in $inputs)
+    {
+        [int[]]$presentsplit = [int[]]($present -split 'x') | sort
+        $side1 = $presentsplit[0] * $presentsplit[1]
+        $side2 = $presentsplit[0] * $presentsplit[2]
+        $side3 = $presentsplit[1] * $presentsplit[2]
+        $smallestside = $side1
+        $total += ($side1 *2) + ($side2 *2) + ($side3 *2) +$smallestside
+    }
+    return $total
+}
+function Get-Part2 ($input)
+{
+    $totalribbon = 0
+    foreach ($present in $inputs)
+    {
+        [int[]]$presentsplit = [int[]]($present -split 'x') | sort
+        $volume = $presentsplit[0] * $presentsplit[1] * $presentsplit[2]
+        $smallestperimeter = ($presentsplit[0] * 2) + ($presentsplit[1] * 2)
+        $totalribbon += ($smallestperimeter + $volume)
+    }
+    return $totalribbon
+}
+Get-Part1 -inputs $inputs
+Get-Part2 -inputs $inputs
