d47a5a4a3093451ab8bf2689ff47a407f82b7c65
VortexUK <30538229+VortexUK@users.noreply.github.com>
2017-10-04 18:24:34 +0100

    Solution for day 1 in python :)

---
+++ 2015/Day1/solution.py
@@ -0,0 +1,30 @@
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+def count_brackets(input):
+    result = input.count('(') - input.count(')')
+    return result
+
+
+def move_elevator(command):
+    if command == '(':
+        return 1
+    if command == ')':
+        return -1
+
+
+def get_basement_floor_index(input):
+    floor = 0
+    index = 0
+    for action in input:
+        floor += move_elevator(action)
+        index += 1
+        if floor == -1:
+            return index
+
+
+inp = file_get_contents("S:\Prod\AdventOfCode\BenM\PyCharm\input.txt")
+print count_brackets(inp)
+print get_basement_floor_index(inp)
