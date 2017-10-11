e36e6592e57cacaa7a76d808f4d0d6d8f4669b23
VortexUK <30538229+VortexUK@users.noreply.github.com>
2017-10-11 19:05:13 +0100

    working solution

--- 2015/Day3/solution.py
+++ 2015/Day3/solution.py
@@ -1,22 +1,51 @@
-from collections import namedtuple
+from collections import Counter
+
+
 def file_get_contents(filename):
     with open(filename) as f:
         return f.read()
 
 
-def move_santa(direction,position):
+def move_santa(direction, position):
     if direction == '^':
-        position.y += 1
+        position[1] += 1
     if direction == 'v':
-        position.y -= 1
+        position[1] -= 1
     if direction == '>':
-        position.x += 1
+        position[0] += 1
     if direction == '<':
-        position.x -= 1
+        position[0] -= 1
     return position
 
-def deliver_presents(input):
-    santa_position = namedtuple("santa_position","x , y")
-    for direction in input
+
+def deliver_presents(directions):
+    santa_position = [0, 0]
+    cnt = Counter()
+    cnt[str(santa_position[0]) + "." + str(santa_position[1])] += 1
+    for direction in directions:
+        santa_position = move_santa(direction, santa_position)
+        cnt[str(santa_position[0]) + "." + str(santa_position[1])] += 1
+    print len(cnt)
+
+
+def deliver_presentsrobo(directions):
+    santa_position = [0, 0]
+    robo_position = [0, 0]
+    cnt = Counter()
+    santasgo = True
+    cnt[str(santa_position[0]) + "." + str(santa_position[1])] += 1
+    cnt[str(robo_position[0]) + "." + str(robo_position[1])] += 1
+    for direction in directions:
+        if santasgo == True:
+            santa_position = move_santa(direction, santa_position)
+            cnt[str(santa_position[0]) + "." + str(santa_position[1])] += 1
+        else:
+            robo_position = move_santa(direction, robo_position)
+            cnt[str(robo_position[0]) + "." + str(robo_position[1])] += 1
+        santasgo = not santasgo
+    print len(cnt)
+
 
 inp = file_get_contents("S:\Prod\AdventOfCode\BenM\PyCharm\Day3.txt")
+deliver_presents(inp)
+deliver_presentsrobo(inp)
