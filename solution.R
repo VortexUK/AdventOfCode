3bf7b740beb659a91dacf94388ead125bd9cbaaf
VortexUK <30538229+VortexUK@users.noreply.github.com>
2017-10-20 12:54:57 +0100

    Delete solution.R

--- solution.R
+++
@@ -1,7 +0,0 @@
-fileName <- 'D:\\git\\AdventOfCode\\2015\\Day1\\input.txt'
-inp <- readChar(fileName, file.info(fileName)$size)
-inpsplit <- unlist(strsplit(inp,""))
-inpsplit_factor <- factor(inpsplit)
-levels(inpsplit_factor) <- c("Up", "Down")
-summary = summary(inpsplit_factor)
-summary[1]-summary[2]
