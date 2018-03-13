c2bdbd6c6efe0de0c896f67706aa541a9f2e1226
VortexUK <30538229+VortexUK@users.noreply.github.com>
2018-03-13 17:29:49 +0000

    completed day 5

---
+++ 2015/Day5/solution.py
@@ -0,0 +1,35 @@
+import re
+
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return [_.strip('\n') for _ in f.readlines()]
+
+
+def test_string(string_input):
+    rule1 = re.compile(r"(?:[aeiou].*){3}", re.IGNORECASE)
+    rule2 = re.compile(r"(.)\1", re.IGNORECASE)
+    rule3 = re.compile(r"AB|CD|PQ|XY", re.IGNORECASE)
+    nice_strings = []
+    for child_string in string_input:
+        if rule1.search(child_string):
+            if rule2.search(child_string):
+                if not rule3.search(child_string):
+                    nice_strings.append(child_string)
+    print len(nice_strings)
+
+
+def test_string2(string_input):
+    nice_strings = []
+    rule4 = re.compile(r"(..).*\1")
+    rule5 = re.compile(r"(.).\1")
+    for child_string in string_input:
+        if rule4.search(child_string):
+            if rule5.search(child_string):
+                nice_strings.append(child_string)
+    print len(nice_strings)
+
+
+inp = file_get_contents("S:\Prod\AdventOfCode\BenM\PyCharm\Day5.txt")
+test_string(inp)
+test_string2(inp)
