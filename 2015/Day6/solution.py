08353d6130d3cf6d5274517abcbb0a9a5c393161
VortexUK <30538229+VortexUK@users.noreply.github.com>
2018-04-03 16:12:57 +0100

    Now working after a few fixes

--- 2015/Day6/solution.py
+++ 2015/Day6/solution.py
@@ -14,21 +14,21 @@ def new_light_grid():
 
 def format_command (unformatted_command):
     split_command = re.split(r"\s+(?!o)",unformatted_command)
-    x_coords = re.split(r",", split_command[1])
-    y_coords = re.split(r",", split_command[3])
+    from_coords = re.split(r",", split_command[1])
+    to_coords = re.split(r",", split_command[3])
     formatted_command = {}
     formatted_command["action"] = split_command[0]
-    formatted_command["from_x"] = int(x_coords[0])
-    formatted_command["to_x"] = int(x_coords[1])
-    formatted_command["from_y"] = int(y_coords[0])
-    formatted_command["to_y"] = int(y_coords[1])
+    formatted_command["from_x"] = int(from_coords[0])
+    formatted_command["to_x"] = int(to_coords[0])
+    formatted_command["from_y"] = int(from_coords[1])
+    formatted_command["to_y"] = int(to_coords[1])
     return formatted_command
 
 
-def invoke_instruction(current_state, unformatted_instruction):
+def invoke_instruction1(current_state, unformatted_instruction):
     inst = format_command(unformatted_instruction)
-    for x in range(inst["from_x"], inst["to_x"]):
-        for y in range(inst["from_y"], inst["to_y"]):
+    for x in range(inst["from_x"], (inst["to_x"] + 1)):
+        for y in range(inst["from_y"], (inst["to_y"] + 1)):
             if inst["action"] == "turn off":
                 current_state[x][y] = 0
             elif inst["action"] == "turn on":
@@ -40,13 +40,32 @@ def invoke_instruction(current_state, unformatted_instruction):
     return current_state
 
 
-def run_instructions(instructions):
+def invoke_instruction2(current_state, unformatted_instruction):
+    inst = format_command(unformatted_instruction)
+    for x in range(inst["from_x"], (inst["to_x"] + 1)):
+        for y in range(inst["from_y"], (inst["to_y"] + 1)):
+            if inst["action"] == "turn off":
+                if current_state[x][y] != 0:
+                    current_state[x][y] -= 1
+            elif inst["action"] == "turn on":
+                current_state[x][y] += 1
+            elif inst["action"] == "toggle":
+                current_state[x][y] += 2
+    return current_state
+
+
+def run_instructions(instructions, mode):
     light_grid = new_light_grid()
-    for instruction in instructions:
-        light_grid = invoke_instruction(light_grid, instruction)
+    if mode == 1:
+        for instruction in instructions:
+            light_grid = invoke_instruction1(light_grid, instruction)
+    if mode == 2:
+        for instruction in instructions:
+            light_grid = invoke_instruction2(light_grid, instruction)
     print sum(sum(light_grid, []))
 
 
 inp = file_get_contents("S:\Prod\AdventOfCode\BenM\PyCharm\Day6.txt")
-run_instructions(inp)
+run_instructions(inp, 1)
+run_instructions(inp, 2)
 
