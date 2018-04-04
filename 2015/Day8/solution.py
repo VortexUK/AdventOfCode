8e0cd65b0bb724e46b39283a216b9cb60cd448c4
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-04-05 00:07:51 +0100

    Added basic functionality for py

---
+++ 2015/Day8/solution.py
@@ -0,0 +1,11 @@
+import re
+import os
+cwd = os.getcwd()
+
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return [_.strip('\n') for _ in f.readlines()]
+
+
+inp = file_get_contents(cwd + "\input.txt")
