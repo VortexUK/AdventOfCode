c14bbacfcafac02f6b27943ed565524b75344eb5
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-04-04 23:59:11 +0100

    finished day 7 in py!

--- 2015/Day7/solution.py
+++ 2015/Day7/solution.py
@@ -1,4 +1,6 @@
 import re
+import os
+cwd = os.getcwd()
 
 
 def file_get_contents(filename):
@@ -44,61 +46,65 @@ def initialise_circuit(starting_inputs):
 
 
 def add_to_circuit(instruction,circuit):
-    if re.match(r"[A-Z]+",instruction["Input_A"]):
-        input_a = circuit[instruction["Input_A"]]
-    else:
-        input_a = instruction["Input_A"]
-    if re.match(r"[A-Z]+",instruction["Input_B"]):
-        input_b = circuit[instruction["Input_B"]]
-    else:
-        input_b = instruction["Input_B"]
     output = instruction["Output"]
-    if instruction["Operation"] == "AND":
-        result = input_a & input_b
-    elif instruction["Operation"] == "OR":
-        result = input_a | input_b
-    elif instruction["Operation"] == "LSHIFT":
-        result = input_a << input_b
-    elif instruction["Operation"] == "RSHIFT":
-        result = input_a >> input_b
-    elif instruction["Operation"] == "NOT":
-        result = 65536 + ~input_a
-    elif instruction["Operation"] == "ASSIGN":
-        result = input_a
-    circuit[output] = result
+    if instruction["Operation"] == "START":
+        circuit[output] = instruction["Input_A"]
+    else:
+        if re.match(r"[A-Za-z]+", instruction["Input_A"]):
+            input_a = int(circuit[instruction["Input_A"]])
+        else:
+            input_a = int(instruction["Input_A"])
+        if instruction["Operation"] != "NOT" and instruction["Operation"] != "ASSIGN":
+            if re.match(r"[A-Za-z]+", instruction["Input_B"]):
+                input_b = int(circuit[instruction["Input_B"]])
+            else:
+                input_b = int(instruction["Input_B"])
+        if instruction["Operation"] == "AND":
+            result = input_a & input_b
+        elif instruction["Operation"] == "OR":
+            result = input_a | input_b
+        elif instruction["Operation"] == "LSHIFT":
+            result = input_a << input_b
+        elif instruction["Operation"] == "RSHIFT":
+            result = input_a >> input_b
+        elif instruction["Operation"] == "NOT":
+            result = 65536 + ~input_a
+        elif instruction["Operation"] == "ASSIGN":
+            result = input_a
+        circuit[output] = result
     return circuit
 
 
 
 def connect_circuit(inputs):
     formatted_inputs = get_commands(inputs)
-    starting_values = filter(lambda x: x["Operation"] == "START", formatted_inputs)
-    circuit = initialise_circuit(starting_values)
-    print circuit
-    # Remove the start instructions:
-    indexes = [i for i, x in enumerate(formatted_inputs) if x["Operation"] == 'START']
-    for index in indexes:
-        del formatted_inputs[index]
+    circuit = {}
     no_end = True
     count = 0
     while no_end:
-        print count
-        count += 1
         for instruction in formatted_inputs:
-            #print instruction
-            if instruction["Input_A"] in circuit.keys() \
-                    and instruction["Operation"] == "NOT":
+            if instruction["Operation"] == "START":
                 circuit = add_to_circuit(instruction, circuit)
                 formatted_inputs.remove(instruction)
-            elif instruction["Input_A"] in circuit.keys() \
-                    and instruction["Input_B"] in circuit.keys() \
-                    and instruction != "NOT":
+            elif (instruction["Input_A"] in circuit.keys() or re.match(r"[0-9]+", instruction["Input_A"])) \
+                    and instruction["Operation"] in ["NOT", "ASSIGN"]:
                 circuit = add_to_circuit(instruction, circuit)
                 formatted_inputs.remove(instruction)
+            elif "Input_B" in instruction.keys():
+                if (instruction["Input_A"] in circuit.keys() or re.match(r"[0-9]+", instruction["Input_A"])) \
+                        and (instruction["Input_B"] in circuit.keys() or re.match(r"[0-9]+", instruction["Input_B"])) \
+                        and instruction != "NOT":
+                    circuit = add_to_circuit(instruction, circuit)
+                    formatted_inputs.remove(instruction)
         if len(formatted_inputs) == 0:
             no_end = False
-            print circuit["a"]
+            print "The answer is: a=" + str(circuit["a"])
+    return str(circuit["a"])
 
 
-inp = file_get_contents("S:\Prod\AdventOfCode\BenM\PyCharm\Day7.txt")
-connect_circuit(inp)
+inp = file_get_contents(cwd + "\input.txt")
+part_1_answer = connect_circuit(inp) # Get the answer to the first Part
+b_matcher = re.compile(".*-> b$")
+current_b = filter(b_matcher.match, inp)
+inp[inp.index(current_b[0])] = part_1_answer + " -> b"
+part_2_answer = connect_circuit(inp) # Get the answer to the second Part
