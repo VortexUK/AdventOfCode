5d77db4eb15b9e15ad1a7bf00c946b9503500560
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-04-05 23:06:57 +0100

    Started on day 8 py

--- 2015/Day8/solution.py
+++ 2015/Day8/solution.py
@@ -8,4 +8,16 @@ def file_get_contents(filename):
         return [_.strip('\n') for _ in f.readlines()]
 
 
+class stringrep:
+
+    firstparta = r'\\\\(?!".)|\\"(?=.)|\\(x[a-z0-9]{2})'
+    firstpartb = r'"'
+    secondpart = r'\\|"'
+
+    def __init__(self,string):
+        self.string = string
+        self.parta = re.sub(self.firstpartb, '', re.sub(self.firstparta, 'a', string))
+        self.partb = re.sub(self.secondpart, 'a', string) # not right - 2/1 length issues
+
+
 inp = file_get_contents(cwd + "\input.txt")
