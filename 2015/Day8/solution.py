2868b2556b725a75c6b27eef81346d9160830dd7
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-04-07 18:19:27 +0100

    class should work for day 8 now

--- 2015/Day8/solution.py
+++ 2015/Day8/solution.py
@@ -10,14 +10,15 @@ def file_get_contents(filename):
 
 class stringrep:
 
-    firstparta = r'\\\\(?!".)|\\"(?=.)|\\(x[a-z0-9]{2})'
+    firstparta = r'\\\\(?!".)|\\"(?=.)|(?<!\\)\\(x[a-z0-9]{2})'
     firstpartb = r'"'
     secondpart = r'\\|"'
 
-    def __init__(self,string):
+    def __init__(self, string):
         self.string = string
         self.parta = re.sub(self.firstpartb, '', re.sub(self.firstparta, 'a', string))
-        self.partb = re.sub(self.secondpart, 'a', string) # not right - 2/1 length issues
+        self.partb = '"' + re.sub(self.secondpart, 'aa', string) + '"'
+
 
 
 inp = file_get_contents(cwd + "\input.txt")
