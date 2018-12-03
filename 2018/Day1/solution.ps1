7c661506ebd0d2b078fef19cb892aaeb24e685c1
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-12-03 11:55:35 +0000

    Solution to 2018 day 1

---
+++ 2018/Day1/solution.ps1
@@ -0,0 +1,26 @@
+﻿$numbers = [System.int64[]](Get-Content -Path D:\git\AdventOfCode\2018\Day1\input.txt)
+#region Part 1
+[System.Int32]$Part1 = ($numbers | Measure-Object -sum).sum
+#endregion
+#region Part 2
+$numberfound = $false
+$currentsum = 0
+$numberhash = @{}
+while (!$Numberfound)
+{
+    :frequencyloop foreach ($number in $numbers)
+    {
+        $currentsum += $number
+        $numberhash.$currentsum += 1
+        if ($numberhash.$currentsum -gt 1)
+        {
+            $numberfound = $true
+            $Part2 = $currentsum
+            break frequencyloop
+            
+        }
+    }
+}
+#endregion
+Write-Host "Answer to Part 1: $Part1"
+Write-Host "Answer to Part 2: $Part2"
