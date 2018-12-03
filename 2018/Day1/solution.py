624bd652105457d2d8da5ec4e758547a15f719b4
Ben McElroy <ben.mcelroy.uk@gmail.com>
2018-12-03 21:24:41 +0000

    Day 1 in python complete :)

--- 2018/Day1/solution.py
+++ 2018/Day1/solution.py
@@ -1,4 +1,3 @@
-import re
 import os
 cwd = os.getcwd()
 
@@ -8,23 +7,22 @@ def file_get_contents(filename):
         return [_.strip('\n') for _ in f.readlines()]
 
 
-def get_frequency_sum(inp):
-    return sum(inp)
+def get_frequency_sum(numbers):
+    return sum(numbers)
+
+
+def get_double_frequency(numbers):
+    frequency_not_found = True
+    seen = list()
+    current_sum = 0
+    while frequency_not_found:
+        for frequency in numbers:
+            current_sum += frequency
+            if current_sum in seen:
+                return current_sum
+            seen.append(current_sum)
 
 
 inp = map(int, file_get_contents(cwd + "\input.txt"))
 print "The Answer to Part 1 is: " + str(get_frequency_sum(inp))
-
-
-frequency_not_found = True
-dict = {}
-current_sum = 0
-while frequency_not_found:
-    for frequency in inp:
-        current_sum += frequency
-        dict[str(current_sum)] += 1
-        if dict[str(current_sum)] > 1:
-            break
-
-
-print current_sum
+print "The Answer to Part 2 is: " + str(get_double_frequency(inp))
